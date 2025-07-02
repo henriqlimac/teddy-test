b512976faa4d9ae7e2454b52443c551374f1ab66
Henrique Lima <h.cardosinho.lima@gmail.com>
2025-07-01 21:11:45 -0300

    chore: base project setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { userReducer } from './shared/store/user.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideStore({ selectedCount: userReducer }),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { CustomersComponent } from './features/customers/customers.component';
+import { LoginComponent } from './features/login/login.component';
+import { SelectedCustomersComponent } from './features/selected-customers/selected-customers.component';
+import { LayoutComponent } from './shared/components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'customers', component: CustomersComponent },
+      { path: 'selected', component: SelectedCustomersComponent },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
